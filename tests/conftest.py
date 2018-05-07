7171e7962786d4a69a7d1bae3cd17e850975bfe8
Mitchell Stokes <mogurijin@gmail.com>
2018-05-06 17:56:25 -0700

    Add an optional, simple AI controller for controlling the second player
    Enable the AI with the 'mercury-use-ai' PRC variable set to true.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,28 @@
+#pylint:disable=redefined-outer-name
 import sys
 import os
 
+import pytest
+
 TESTDIR = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, os.path.join(TESTDIR, '..', 'game'))
+
+
+@pytest.fixture
+def combatant():
+    import panda3d.core as p3d
+    import combatant
+    import gamedb
+
+    breed = gamedb.GameDB.get_instance()['breeds']['bobcatshark']
+
+    return combatant.Combatant(breed, p3d.NodePath(''), [])
+
+@pytest.fixture
+def dt():
+    return 1/60
+
+@pytest.fixture
+def ai_controller(combatant):
+    import ai
+    return ai.Controller(combatant)
