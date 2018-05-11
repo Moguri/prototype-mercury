9b61ad15f7215b6c5d7560ff4089730eaa4ea4a3
Mitchell Stokes <mogurijin@gmail.com>
2018-05-10 19:24:43 -0700

    effects: Use an object to store effect state while building Sequences
    This helps keep the ever growing list of function arguments under
    control.

--- game/effects.py
+++ game/effects.py
@@ -4,66 +4,6 @@ import panda3d.core as p3d
 from direct.interval import IntervalGlobal as intervals
 
 
-def change_stat(_combatant, target, hit, strength, parameters, _rendernp):
-    stat = parameters['stat']
-    def func():
-        if hit:
-            setattr(target, stat, getattr(target, stat) - strength)
-    return intervals.Func(func)
-
-
-def show_result(_combatant, target, hit, strength, _parameters, rendernp):
-    textnode = p3d.TextNode('effect result')
-    textnode.set_align(p3d.TextNode.ACenter)
-    textnode.set_text(str(strength) if hit else "Miss")
-
-    textnp = rendernp.attach_new_node(textnode)
-    textnp.set_pos(target.path, 0, 0, 2)
-    textnp.set_billboard_point_eye()
-    textnp.set_bin("fixed", 0)
-    textnp.set_depth_test(False)
-    textnp.set_depth_write(False)
-    textnp.set_scale(0.5)
-    textnp.hide()
-
-    def func():
-        intervals.Sequence(
-            intervals.Func(textnp.show),
-            intervals.LerpPosInterval(
-                textnp,
-                1.0,
-                textnp.get_pos() + p3d.LVector3(0, 0, 0.5)
-            ),
-            intervals.Func(textnp.remove_node),
-        ).start()
-
-    return intervals.Func(func)
-
-
-def play_animation(combatant, target, _hit, _strength, parameters, _rendernp):
-    return target.path.actor_interval(
-        combatant.get_anim(parameters['animation_name'])
-    )
-
-
-def template_simple(combatant, target, hit, strength, parameters, rendernp):
-    if 'stat' not in parameters:
-        parameters['stat'] = 'current_hp'
-    return intervals.Sequence(
-        play_animation(combatant, combatant, hit, strength, parameters, rendernp),
-        show_result(combatant, target, hit, strength, parameters, rendernp),
-        change_stat(combatant, target, hit, strength, parameters, rendernp),
-    )
-
-
-_EFFECT_MAP = {
-    'change_stat': change_stat,
-    'play_animation': play_animation,
-    'show_result': show_result,
-    'template_simple': template_simple,
-}
-
-
 BP_HITMOD = [
     [100, 10],
     [200, 20],
@@ -129,34 +69,115 @@ def calculate_strength(combatant, target, ability):
 
     return round(base_str * str_factor * def_factor)
 
+class SequenceBuilder:
+    ALLOWED_EFFECTS = [
+        'change_stat',
+        'show_result',
+        'play_animation',
 
-def sequence_from_ability(rendernp, combatant, ability):
-    sequence = intervals.Sequence()
+        'template_simple',
+    ]
 
-    hit_chance = calculate_hit_chance(combatant, combatant.target, ability)
-    roll = random.randrange(0, 99)
-    hit = hit_chance > roll
-    #print(hit, hit_chance, die)
-    strength = calculate_strength(combatant, combatant.target, ability)
+    def __init__(self, rendernp, combatant, ability):
+        self.rendernp = rendernp
+        self.combatant = combatant
+        self.ability = ability
 
-    for effect in ability.effects:
+        hit_chance = calculate_hit_chance(combatant, combatant.target, ability)
+        roll = random.randrange(0, 99)
+        self.is_hit = hit_chance > roll
+        #print(hit, hit_chance, die)
+        self.strength = calculate_strength(combatant, combatant.target, ability)
+
+        self.sequence = intervals.Sequence()
+
+        for effect in ability.effects:
+            self.sequence.extend(self.parse_effect(effect))
+
+    def parse_effect(self, effect):
+        sequence = intervals.Sequence()
         target = effect.get('target', 'other')
         if target == 'self':
-            target = combatant
+            target = self.combatant
+            self.initial_position = self.initial_self_position
         elif target == 'other':
-            target = combatant.target
+            target = self.combatant.target
+            self.initial_position = self.initial_other_position
         else:
             raise RuntimeError("Unkown effect target: {}".format(target))
 
         parameters = effect.get('parameters', {})
         etype = effect['type']
 
-        if etype not in _EFFECT_MAP:
+        if etype not in self.ALLOWED_EFFECTS:
             raise RuntimeError("Unknown effect type: {}".format(etype))
 
-        sequence.append(_EFFECT_MAP[etype](combatant, target, hit, strength, parameters, rendernp))
+        sequence.append(getattr(self, etype)(target, parameters))
 
-    return sequence
+        return sequence
+
+    def as_sequence(self):
+        return self.sequence
+
+    #
+    # Basic Effects
+    #
+    def change_stat(self, target, parameters):
+        stat = parameters['stat']
+        def func():
+            if self.is_hit:
+                setattr(target, stat, getattr(target, stat) - self.strength)
+        return intervals.Func(func)
+
+
+    def show_result(self, target, _parameters):
+        textnode = p3d.TextNode('effect result')
+        textnode.set_align(p3d.TextNode.ACenter)
+        textnode.set_text(str(self.strength) if self.is_hit else "Miss")
+
+        textnp = self.rendernp.attach_new_node(textnode)
+        textnp.set_pos(target.path, 0, 0, 2)
+        textnp.set_billboard_point_eye()
+        textnp.set_bin("fixed", 0)
+        textnp.set_depth_test(False)
+        textnp.set_depth_write(False)
+        textnp.set_scale(0.5)
+        textnp.hide()
+
+        def func():
+            intervals.Sequence(
+                intervals.Func(textnp.show),
+                intervals.LerpPosInterval(
+                    textnp,
+                    1.0,
+                    textnp.get_pos() + p3d.LVector3(0, 0, 0.5)
+                ),
+                intervals.Func(textnp.remove_node),
+            ).start()
+
+        return intervals.Func(func)
+
+
+    def play_animation(self, target, parameters):
+        return target.path.actor_interval(
+            target.get_anim(parameters['animation_name'])
+        )
+
+    #
+    # Template Effects
+    #
+    def template_simple(self, target, parameters):
+        if 'stat' not in parameters:
+            parameters['stat'] = 'current_hp'
+        return intervals.Sequence(
+            self.play_animation(self.combatant, parameters),
+            self.show_result(target, parameters),
+            self.change_stat(target, parameters),
+        )
+
+
+def sequence_from_ability(rendernp, combatant, ability):
+    return SequenceBuilder(rendernp, combatant, ability).as_sequence()
 
 
 def _test():
