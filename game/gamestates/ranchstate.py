d85180a198b460261cd7ab6c7a2f6bec6de6335a
Mitchell Stokes <mogurijin@gmail.com>
2018-05-29 19:53:43 -0700

    ranch: Add stat raising menu

--- game/gamestates/ranchstate.py
+++ game/gamestates/ranchstate.py
@@ -18,14 +18,29 @@ class RanchState(GameState):
             self.player.monster = random.choice(list(gdb['monsters'].values()))
             print("Assigned monster: {}".format(self.player.monster.name))
 
-        self.menu_items = [
-            ('Combat', self.enter_combat, []),
-        ]
+        self.menus = {
+            'base': [
+                ('Combat', self.enter_combat, []),
+                ('Train', self.set_menu, ['training']),
+            ],
+            'training': [
+                ('Back', self.set_menu, ['base']),
+                ('Hit Points', self.train_stat, ['hp']),
+                ('Physical Attack', self.train_stat, ['physical_attack']),
+                ('Magical Attack', self.train_stat, ['magical_attack']),
+                ('Accuracy', self.train_stat, ['accuracy']),
+                ('Evasion', self.train_stat, ['evasion']),
+                ('Defense', self.train_stat, ['defense']),
+            ],
+        }
+        self.menu_items = None
         self.selection_idx = 0
+        self.set_menu('base')
 
         self.accept('p1-move-down', self.increment_selection)
         self.accept('p1-move-up', self.decrement_selection)
         self.accept('p1-accept', self.accept_selection)
+        self.accept('p1-reject', self.set_menu, ['base'])
 
         self.load_ui('ranch')
         self.update_ui({
@@ -58,3 +73,18 @@ class RanchState(GameState):
             self.player.monster.id
         ]
         base.change_state('Combat')
+
+    def set_menu(self, new_menu):
+        self.menu_items = self.menus[new_menu]
+        self.selection_idx = 0
+        self.update_ui({
+            'menu_items': [i[0] for i in self.menu_items],
+        })
+
+    def train_stat(self, stat):
+        stat_growth = 10
+        attr = '{}_offset'.format(stat)
+        old_stat = getattr(self.player.monster, attr)
+        setattr(self.player.monster, attr, old_stat + stat_growth)
+
+        print(self.player.monster)
