c8c1d35e63194eaaee08ba7077ff4a5d9bf0509a
Mitchell Stokes <mogurijin@gmail.com>
2020-04-04 23:36:50 -0700

    Combat: Add basic AI for the enemy combatants

--- game/gamestates/combatstate.py
+++ game/gamestates/combatstate.py
@@ -90,6 +90,90 @@ class Arena:
                 tilenp.set_color(color)
 
 
+class AiController():
+    def __init__(self, arena, controller, effects_root):
+        self.arena = arena
+        self.controller = controller
+        self.effects_root = effects_root
+
+    def update_combatant(self, combatant, enemy_combatants):
+        # Find a target
+        target = enemy_combatants[0]
+        for enemy in enemy_combatants:
+            dist_to_closest = self.arena.tile_distance(
+                combatant.tile_position,
+                target.tile_position
+            )
+            dist_to_current = self.arena.tile_distance(
+                combatant.tile_position,
+                enemy.tile_position
+            )
+            if dist_to_current < dist_to_closest:
+                target = enemy
+
+        # Pick an ability
+        ability = random.choice(target.abilities)
+
+        # Find a tile to move to
+        tiles = self.arena.find_tiles_in_range(
+            combatant.tile_position,
+            0,
+            combatant.movement
+        )
+        target_tile = None
+        dist_to_target = self.arena.tile_distance(
+            combatant.tile_position,
+            target.tile_position
+        )
+        for tile in tiles:
+            tile_dist = self.arena.tile_distance(
+                tile,
+                target.tile_position
+            )
+            if ability.range_min <= tile_dist <= ability.range_max:
+                target_tile = tile
+                dist_to_target = tile_dist
+                break
+            elif tile_dist < dist_to_target:
+                target_tile = tile
+                dist_to_target = tile_dist
+
+        if target_tile:
+            self.controller.move_combatant_to_tile(combatant, target_tile)
+
+        # Update facing
+        facing = self.arena.tile_get_facing_to(
+            combatant.tile_position,
+            target.tile_position
+        )
+        if sum(facing) != 0:
+            combatant.path.set_h(
+                math.degrees(math.atan2(facing[1], facing[0])) - 90
+            )
+
+        # Use an ability if able
+        sequence = intervals.Sequence()
+        if ability.range_min <= dist_to_target <= ability.range_max:
+            self.controller.display_message(
+                f'{combatant.name} is using {ability.name} '
+                f'on {target.name}'
+            )
+            combatant.target = target
+            target.target = combatant
+            sequence = effects.sequence_from_ability(
+                self.effects_root,
+                combatant,
+                ability,
+                self.controller
+            )
+            sequence.extend([
+                intervals.Func(self.controller.check_for_dead_combatants),
+                intervals.WaitInterval(1),
+            ])
+
+        return sequence
+
+
 class CombatState(GameState):
     SELECTED_COLOR = (3, 0, 0, 1)
     RANGE_COLOR = (0, 0, 3, 1)
@@ -149,6 +233,9 @@ class CombatState(GameState):
         # Setup UI
         self.load_ui('combat')
 
+        # Setup AI
+        self.aicontroller = AiController(self.arena, self, self.root_node)
+
         # Set initial input state
         self.input_state = 'END_TURN'
 
@@ -193,12 +280,15 @@ class CombatState(GameState):
             ] + [
                 (ability.name, use_ability, [ability])
                 for ability in self.current_combatant.abilities
+            ] + [
+                ('End Turn', self.set_input_state, ['END_TURN']),
             ])
             def update_ability(idx):
-                if idx == 0:
-                    self.selected_ability = None
-                else:
+                try:
                     self.selected_ability = self.current_combatant.abilities[idx - 1]
+                except IndexError:
+                    # Not an ability option
+                    self.selected_ability = None
             self.menu_helper.selection_change_cb = update_ability
             def action_reject():
                 self.input_state = 'SELECT'
@@ -248,14 +338,12 @@ class CombatState(GameState):
                         self
                     )
                     def cleanup():
-                        for combatant in (self.current_combatant, selection):
-                            if combatant.is_dead:
-                                combatant.play_anim('death')
-                        self.current_combatant.current_ct = 0
-                        self.input_state = 'END_TURN'
-                    sequence.extend(intervals.Sequence(
+                        self.check_for_dead_combatants()
+                        if self.input_state != 'END_COMBAT':
+                            self.input_state = 'END_TURN'
+                    sequence.append(
                         intervals.Func(cleanup)
-                    ))
+                    )
                     sequence.start()
 
             def reject_target():
@@ -266,6 +354,8 @@ class CombatState(GameState):
             self.display_message('Select a target')
         elif next_state == 'END_TURN':
             self.selected_ability = None
+            if self.current_combatant:
+                self.current_combatant.current_ct = 0
             combatants_by_ct = sorted(
                 list(self.combatants),
                 reverse=True,
@@ -277,7 +367,21 @@ class CombatState(GameState):
                 for combatant in self.combatants:
                     combatant.current_ct += ctdiff
             self.selected_tile = self.current_combatant.tile_position
-            self.input_state = 'ACTION'
+
+            if self.current_combatant in self.player_combatants:
+                self.input_state = 'ACTION'
+            else:
+                sequence = self.aicontroller.update_combatant(
+                    self.current_combatant,
+                    self.player_combatants
+                )
+                def cleanup():
+                    if self.input_state != 'END_COMBAT':
+                        self.input_state = 'END_TURN'
+                sequence.extend([
+                    intervals.Func(cleanup),
+                ])
+                sequence.start()
         elif next_state == 'END_COMBAT':
             if not self.get_remaining_enemy_combatants():
                 self.display_message('Victory!')
@@ -352,11 +456,22 @@ class CombatState(GameState):
             if not i.is_dead
         ]
 
+    def combat_over(self):
+        return (
+            not self.get_remaining_player_combatants() or
+            not self.get_remaining_enemy_combatants()
+        )
+
+    def check_for_dead_combatants(self):
+        for combatant in self.player_combatants + self.enemy_combatants:
+            if combatant.is_dead:
+                combatant.play_anim('death')
+
     def update(self, dt):
         super().update(dt)
 
         # Check for end condition
-        if not self.get_remaining_player_combatants() or not self.get_remaining_enemy_combatants():
+        if self.combat_over():
             self.input_state = 'END_COMBAT'
 
         # Update combatant facing
