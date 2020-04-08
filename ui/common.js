dc006e70c4d26d221a559b368d0ebfb875b906f3
Mitchell Stokes <mogurijin@gmail.com>
2020-04-07 19:09:38 -0700

    ui: Pull common nav-bar and message-bar logic into Vue components

--- ui/common.js
+++ ui/common.js
@@ -20,3 +20,27 @@ function update_state(new_state) {
     data.state = Object.assign({}, data.state, new_state); 
 }
 
+Vue.component('nav-bar', {
+  props: ['show_menu', 'menu_heading', 'menu_items', 'selection_index'],
+  template: `
+      <div id='nav' v-if='show_menu' class='pure-menu'>
+          <span class='custom-menu-heading pure-menu-heading'>{{menu_heading}}</span>
+          <ul class='pure-menu-list'>
+              <li v-for='(item, idx) in menu_items' class='custom-menu-item pure-menu-item'
+                  v-bind:class="{'pure-menu-selected': idx == selection_index}">
+                  <a href='#' class='pure-menu-link'>{{item}}</a>
+              </li>
+          </ul>
+      </div>
+  `
+});
+
+Vue.component('message-bar', {
+  props: ['message', 'modal'],
+  template: `
+    <div id='message-bar' v-if='message !== ""'>
+        <span>{{message}}</span>
+        <span class='modal-indicator' v-if='modal'>&#x25bc;</span>
+    </div>
+  `
+});
