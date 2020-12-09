8254fa24180586e6e2c6fe4cbfe8d68e86e141bb
Andrew Smith <smithandrew.1988@gmail.com>
2020-12-08 20:35:07 -0800

    Update descriptions and names

--- game/effects.py
+++ game/effects.py
@@ -28,7 +28,7 @@ def calculate_strength(combatant, ability):
     else:
         ability_power = ability.power
 
-    return round(attack_stat + ability_power)
+    return attack_stat + ability_power
 
 class SequenceBuilder:
     ALLOWED_EFFECTS = [
@@ -136,9 +136,11 @@ class SequenceBuilder:
     #
     def change_stat(self, target, parameters):
         stat = parameters['stat']
-        local_str_fac = parameters.get('strength_factor', 0)
+        local_str_fac = parameters.get('strength_factor', 1)
         seq = intervals.Sequence()
-        strength = self.strength + local_str_fac
+        strength = max(round(abs(strength) * 0.1), 1)
+        if self.strength * local_str_fac < 0:
+            strength *= -1
         if local_str_fac < 0:
             strength = local_str_fac * -1
         if parameters.get('show_result', True):
